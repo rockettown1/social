6b7ccfef29d3144229aa498d1d8cb05af2b28261
Dan Krishnan <dan.krishnan01@gmail.com>
2019-04-20 23:10:16 +0100

    added some front end React - landing, register and login

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
 //setup port
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
